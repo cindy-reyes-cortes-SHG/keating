fc8d6643dc086ac2ecb24eaa648e7195fa9c27bf
Cindy Reyes-Cortes <cindy-reyes-cortes@sandhills.com>
2026-04-08 15:45:37 -0500

    homepage completed

--- js/slick-custom.js
+++ js/slick-custom.js
@@ -4,28 +4,14 @@ $(document).ready(function(){
   infinite: true,
   slidesToShow: 1,
   slidesToScroll: 1,
+  dots: true,
 	autoplay:true,
   arrows: false,
   prevArrow: '<button type="button" class="slick-button slick-prev"><i class="fa-solid fa-arrow-left-long"></i> Previous </button>',
   nextArrow: '<button type="button" class="slick-button slick-next">Next <i class="fa-solid fa-arrow-right-long"></i></button>',
 	autoplaySpeed: 5000,
 
-responsive: [
-    {
-      breakpoint: 775,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
-      }
-    },
-    {
-      breakpoint: 545,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-  ]
+
 });
 	
    });
