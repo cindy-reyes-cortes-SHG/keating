6bc51251f768f4aaeb9d6fad38a6c0a669222091
Cindy Reyes-Cortes <cindy-reyes-cortes@sandhills.com>
2026-04-09 14:34:17 -0500

    schedule service page

--- js/slick-custom.js
+++ js/slick-custom.js
@@ -13,5 +13,19 @@ $(document).ready(function(){
 
 
 });
+
+
+	$('.category-equipment-pills').slick({
+  infinite: true,
+  slidesToShow: 2,
+  slidesToScroll: 1,
+	autoplay:true,
+  prevArrow: '<button type="button" class="slick-button slick-prev"><i class="fa-solid fa-arrow-left-long"></i> Previous </button>',
+  nextArrow: '<button type="button" class="slick-button slick-next">Next <i class="fa-solid fa-arrow-right-long"></i></button>',
+	autoplaySpeed: 5000,
+  variableWidth: true
+});
+
+
 	
    });
